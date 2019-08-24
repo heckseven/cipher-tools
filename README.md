d619e75251df966990b3c788810038bceed56282
heckseven <54460388+heckseven@users.noreply.github.com>
2019-08-24 17:05:06 -0500

    Added Frequency analysis

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cipher-tools
  Heckin tools to help decode various ciphers
+
+## Python tools
+I'm starting by creating some python scripts to automate the not so fun stuff.
+
+* `frequency.py` does some really basic frequency analysis of a string or the contents of a selected file. Options to remove whitespace and set the number of characters. Can also run analysis multiple times on the same string/file with different settings.
---
+++ python/frequency.py
@@ -0,0 +1,46 @@
+import re
+from collections import Counter
+
+ciphertext = ""
+
+def getContent():
+    cipherType = input("Analyze 'string' or 'file'? ")
+    if cipherType == "string":
+        return input("Enter the string to evaluate: ")
+    elif cipherType == "file":
+        file = open(input("Enter the file path: "), "r")
+        ciphertext = file.read()
+        file.close()
+        return ciphertext
+    else:
+        print("Please enter 'string' or 'file':")
+        getContent()
+
+def removeWhitespace(string):
+    if input("Remove spaces and newlines from cipher? (yes/no) ") == "yes":
+        string = re.sub(r"[ ]*[\n]*", "", string)
+        return string
+    else:
+        return string
+
+def analyze(cipher):
+    ocipher = cipher
+    cipher = removeWhitespace(cipher)
+    count =  int(input("Enter the segment length to analyze: "))
+    if count != 1:
+        cipher = [cipher[i:i+count] for i in range(0, len(cipher), count)]
+    content=Counter(cipher).most_common()
+    readout = {}
+    print("")
+    for i in content:
+        if i[1] in readout:
+            readout[i[1]].append(i[0])
+        else:
+            readout[i[1]] = []
+            readout[i[1]].append(i[0])
+    for i in readout:
+        print(str(i) + " - " + str(readout[i]))
+    if input("\nContinue frequency analysis on this cypher? (yes/no) ") == "yes":
+        analyze(ocipher)
+
+analyze(getContent())
